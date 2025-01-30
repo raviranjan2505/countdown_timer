5940ec539169496fc63e55145ae14e065e44e4b6
Ravi Ranjan <raviranjan2505@gmail.com>
2025-01-30 13:53:55 +0530

    stop watch using nasted if and setinterval

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import StopWatch from './components/StopWatch'
+
+function App() {
+ 
+  return (
+    <>
+         <StopWatch />                                                                                                                     
+    </>
+  )
+}
+
+export default App
